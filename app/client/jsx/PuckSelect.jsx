6fb01aba1042aa7414d966d5c91d89a7161fe12c
danielmdavis <danis1911@gmail.com>
2020-05-16 13:50:11 -0400

    style

--- app/client/jsx/PuckSelect.jsx
+++ app/client/jsx/PuckSelect.jsx
@@ -1,37 +1,38 @@
-import React, { Component } from 'react';
-import PuckBox from './PuckBox.jsx';
+import React, { Component } from 'react'
+import PuckBox from './PuckBox.jsx'
 import { Avatars } from './avatars.jsx'
 
-
 class PuckSelect extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            columnOpen: false,
-            rowOpen: null,
-            avatarDesign: '',
-            avatarColorway: '',
-            opacity: this.props.opacity
-        }
-        this.onButtonClick = this.onButtonClick.bind(this)
-        this.onButtonClickReset = this.onButtonClickReset.bind(this)
-        this.handleClickDesign = this.handleClickDesign.bind(this)
-        this.handleClickColor = this.handleClickColor.bind(this)
-      }
+  constructor (props) {
+    super(props)
+    this.state = {
+      columnOpen: false,
+      rowOpen: null,
+      avatarDesign: '',
+      avatarColorway: '',
+      opacity: this.props.opacity
+    }
+    this.onButtonClick = this.onButtonClick.bind(this)
+    this.onButtonClickReset = this.onButtonClickReset.bind(this)
+    this.handleClickDesign = this.handleClickDesign.bind(this)
+    this.handleClickColor = this.handleClickColor.bind(this)
+  }
 
-  onButtonClick() {
-    this.setState({ columnOpen: true }) }
+  onButtonClick () { this.setState({ columnOpen: true }) }
 
-  onButtonClickReset() {
+  onButtonClickReset () {
     this.setState({ columnOpen: false })
     this.setState({ rowOpen: null })
     this.setState({ avatarDesign: '' })
     this.setState({ avatarColorway: '' })
     this.setState({ columnOpen: true })
   }
-  handleClickDesign(key, index) {
-    this.setState({ avatarDesign: key, rowOpen: index }) }
-  handleClickColor(variantKey) {
+
+  handleClickDesign (key, index) {
+    this.setState({ avatarDesign: key, rowOpen: index })
+  }
+
+  handleClickColor (variantKey) {
     this.setState({ avatarColorway: variantKey })
     this.props.handleSelect({
       type: this.state.avatarDesign,
@@ -39,12 +40,9 @@ class PuckSelect extends Component {
     })
   }
 
-  render() {
+  render () {
     if (this.state.avatarColorway === '') { // checks whether selection not yet complete
-
-      // puck list generated from purple of each design
-
-      // Object.keys(Avatars).forEach(function(key) { console.log( Avatars[key].purple ) })
+      // puck list generated from the purple of each design
       let allAvatarDesigns = []
       let allColorwayVariants = []
       let fade = this.props.opacity
@@ -53,37 +51,37 @@ class PuckSelect extends Component {
         fade = 'fade'
         allAvatarDesigns = Object.keys(Avatars).map((key, index) => {
           if (this.state.rowOpen !== index) { // checks each item in column for whether selected
-            let handleClick = () => this.handleClickDesign(key, index)
-            let image_transparency = 'image'
+            const handleClick = () => this.handleClickDesign(key, index)
+            let imageTransparency = 'image'
             if (this.state.rowOpen !== null && this.state.rowOpen !== index) {
-              image_transparency = 'non-selected-image'
+              imageTransparency = 'non-selected-image'
             }
 
             return ( // returns single instance of each nonselected design
               <PuckBox
-              handleClick={handleClick}
-              key={key}
-              image={Avatars[key].purple}
-              style='box'
-              imageStyle={image_transparency}
+                handleClick={handleClick}
+                key={key}
+                image={Avatars[key].purple}
+                style='box'
+                imageStyle={imageTransparency}
               />
             )
           } else { // selected design
             allColorwayVariants = Object.keys(Avatars[key]).map((variantKey, colorIndex) => {
-              let selected = 'image'
-              let handleClick = () => this.handleClickColor(variantKey, colorIndex)
+              const selected = 'image'
+              const handleClick = () => this.handleClickColor(variantKey, colorIndex)
               return ( // returns all colorways for selected design
                 <PuckBox
-                handleClick={handleClick}
-                key={variantKey}
-                image={Avatars[key][variantKey]}
-                imageStyle={selected}
+                  handleClick={handleClick}
+                  key={variantKey}
+                  image={Avatars[key][variantKey]}
+                  imageStyle={selected}
                 />
               )
             })
             return ( // colorways row in place of selected column item
               <div className='inner'>
-              {allColorwayVariants}
+                {allColorwayVariants}
               </div>
             )
           }
@@ -91,18 +89,18 @@ class PuckSelect extends Component {
       }
       return ( // column of singleton nonselected items with at most one of them swapped out for colorway variants row
         <div className='outer'>
-        <input className={fade} type="button" onClick={this.onButtonClick} value="Pick your puck"/>
-        {allAvatarDesigns}
+          <input className={fade} type='button' onClick={this.onButtonClick} value='Pick your puck' />
+          {allAvatarDesigns}
         </div>
       )
     } else {
       return ( // when avatar state isn't null, collapse all and display choice
         <div className='outer'>
-          <input className='fade' type="button" onClick={this.onButtonClickReset} value="Pick your puck"/>
-          <div className='spacer'/>
-          <div><img className='image' src={Avatars[this.state.avatarDesign][this.state.avatarColorway]}/></div>
+          <input className='fade' type='button' onClick={this.onButtonClickReset} value='Pick your puck' />
+          <div className='spacer' />
+          <div><img className='image' src={Avatars[this.state.avatarDesign][this.state.avatarColorway]} /></div>
         </div>
-      ) // reselection WIP
+      )
     }
   }
 }
